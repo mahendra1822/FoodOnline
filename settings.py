5ec71915ea09b20e546152f85d00c4b6b8e73098
mahendra zalte <mahendra22zalte@gmail.com>
2022-12-09 12:43:09 +0530

    Register user funtionalities and homepage setup

--- settings.py
+++ settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     }
 }
 
-# AUTH_USER_MODEL = 'accounts.User'
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Password validation
@@ -136,3 +136,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
